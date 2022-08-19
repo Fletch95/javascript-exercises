b8846b6e9802016baf5dcf5b46298f390618d1e0
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-08-19 10:56:40 +0100

    Solve up to 04 part 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, nm) {
+    let output = "";
 
+    if (nm < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < nm; i++) {
+        output += str;
+    };
+
+    return output;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ids) {
 
+    array.splice(array.indexOf(ids),1)
+
+     
+    return array
 };
 
 // Do not edit below this line
