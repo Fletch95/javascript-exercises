76935bbfa8d3d20d7b1c8a13a14a189410befadb
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-08-19 12:06:08 +0100

    Solve up to 04 part 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,25 @@
-const removeFromArray = function(array, ids) {
+const removeFromArray = function() {
+    // First argument must be an array, remaining arguments will be removed from that array.
+    const args = Array.from(arguments);
 
-    array.splice(array.indexOf(ids),1)
+    let array = args[0]; // Selecting the array from the first element (array.from?)
 
-     
+    console.log(`Length of array: ${args.length}`)
+
+    if (args.length == 2) {
+        console.log(`Removing ${args[1]}`)
+        array.splice(array.indexOf(args[1]),1)
+    } else {
+        const ids = args.slice(1,);
+
+        console.log(`Items to be removed: ${ids}`);
+        
+        for (const id of ids) {
+            console.log(`Removing ${id}`)
+            array.splice(array.indexOf(id),1);
+        }        
+    }
+
     return array
 };
 
