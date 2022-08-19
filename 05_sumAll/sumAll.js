8b840eaaa759ec1f70581615e22b8167b2f620e6
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-08-19 12:31:23 +0100

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (start > end) { // Reverse if start is greater than end
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    let sum = 0;
+
+    for (let index = start; index <= end; index++) {
+        sum += index;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
