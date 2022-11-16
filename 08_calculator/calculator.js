efada411e46a6d441d4bc570bf5707e71f001145
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-11-16 21:07:52 +0000

    Solution for 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let running = 0;
+
+  for (const element of array) {
+    running += element;
+  }
+
+  return running;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let running = 1;
 
+  for (const element of array) {
+    running *= element;
+  }
+
+  return running;
 };
 
-const power = function() {
-	
+const power = (a, b) => {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a <= 1) {
+    return 1;
+  } else {
+    running = 1;
+    for(a; a > 1; a--){
+      running *= a;
+    }
+    return running;
+  }
 };
 
 // Do not edit below this line
