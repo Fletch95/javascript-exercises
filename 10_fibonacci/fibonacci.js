e2976eb256888600d790a10ee8d1734b34b545e8
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-11-16 21:39:00 +0000

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+    if (a < 1) {
+        return "OOPS";
+    } else {
+        fib = [1, 1]
+        for (let i = 2; i < a; i++) {
+            fib.push(fib[i - 1] + fib[i - 2]);
+        }
+        return fib[a - 1];
+    }
 };
 
 // Do not edit below this line
