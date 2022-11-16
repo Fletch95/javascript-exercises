62a895b0495ef3bf1247f9271cf435820903dc60
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-11-16 22:09:22 +0000

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    year = new Date().getFullYear();
 
+    const ordered = array.sort(function(a, b) {
+        if (!("yearOfDeath" in a)) {
+            a.yearOfDeath = year;
+        };
+
+        if (!("yearOfDeath" in b)) {
+            b.yearOfDeath = year;
+        };
+
+        return a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1;
+    });
+
+    console.table(ordered);
+    return ordered[0];
 };
 
 // Do not edit below this line
