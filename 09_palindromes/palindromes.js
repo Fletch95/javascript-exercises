8f7dcedb87a17f907645af006e494b2723d7d29a
Thomas Fletcher <t_fletcher@hotmail.co.uk>
2022-11-16 21:23:39 +0000

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //strip punctuation
+    word = word.replace(/\W|_/g, '');
+    word = word.toLowerCase();
 
+    //create reverse word
+    let reverse = word.split("").reverse().join("");
+
+    return(word === reverse);
 };
 
 // Do not edit below this line
